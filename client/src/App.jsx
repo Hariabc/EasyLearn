84adf75aa1939dcc9915c1eb4385909aa7c1773e
Hariabc <119649903+Hariabc@users.noreply.github.com>
2025-05-13 10:34:23 +0530

    ..

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SignIn from "./components/Logins/Signin";
+import SignUp from "./components/Logins/Signup";
 import HeroSection16 from "./components/Navbar";
 import FeatureSection from "./components/Sample";
 import CoursesPage from "./pages/CoursesPage";
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HeroSection16/>}></Route>
         <Route path="/login" element={<SignIn />} />
+        <Route path="/register" element={<SignUp />} />
         <Route path="/dashboard" element={<Dashboard/>} />
       </Routes>
     </Router>
