77d44f61bd1263305d1ed97500926521680f33af
VenuMadhav21 <venumadhav721@gmail.com>
2025-05-13 22:00:49 +0530

    Revert "Backend folder added"
    This reverts commit a11e81fe1ebb87959c2644ac9a31f91448ca25dc.

--- Backend/models/student.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const studentSchema = new mongoose.Schema({
-  name: String,
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true }
-});
-
-module.exports = mongoose.model('Student', studentSchema);
